db19eb40154d18ac9baeb393c5f812adbd195bb0
thomaschhh <holz.thomas@yahoo.de>
2021-04-16 23:14:42 +0200

    changed sampling_rate assertion

--- spheadpy_test.py
+++ spheadpy_test.py
@@ -1,9 +1,4 @@
 # %%
-# Write exponential sweep to csv for testing.
-# The sweep was manually inspected.
-# The time signal was inspected for smootheness and maximum amplitudes of +/-1.
-# The spectrum was inspected for the ripple at the edges of the frequency range
-# (typical for time domain sweep generation) and the 1/f slope.
 import numpy as np
 
 import numpy.testing as npt
@@ -14,7 +9,7 @@ from pyfar import Signal
 from pyfar.coordinates import Coordinates
 from pyfar.spatial import samplings
 
-from spheadpy import AKsphericalHead
+from HRTF import AKsphericalHead
 
 def test_sph_great_circle_with_defaults():
     """Test sph_great_circle with default parameters."""
@@ -25,16 +20,16 @@ def test_sph_great_circle_with_defaults():
     assert isinstance(sg, Coordinates)
 
 def test_AKsphericalHead_with_defaults():
-    """Test exponential sweep against manually verified reference."""
+    """Test AKsphericalHead with default paramters."""
     sg = samplings.sph_great_circle(elevation=np.linspace(-90, 90, 19), gcd=10, radius=1,
                                     azimuth_res=1, match=360)
 
     shtf, offCenterParameter = AKsphericalHead(sg, ear = [85, -13], offCenter = False, r_0 = None, 
                                                     a = 0.0875, Nsh = 100, Nsamples = 1024, fs = 44100, c = 343)
 
-    assert offCenterParameter == "False"
+    assert offCenterParameter == False
 
     assert shtf.cshape == (1024, )
-    assert shtf.sampling_rate == 4410
+    assert shtf.sampling_rate == 44100
     assert shtf.fft_norm == "none"
     
